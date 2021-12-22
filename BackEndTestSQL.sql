3a3857430c7cc91bcd31afd98b6a479be2e78e78
VEpardo <93387563+VEpardo@users.noreply.github.com>
2021-12-21 19:47:31 -0600

    Added SQL Sentences and Store Procedures
    5. Create SQLSentence  joining the 2 tables.
    6. Create SQL Sentence  grouping  by Languages.
    7. Create Store Procedures (Insert,Update,Delete,Select all)

--- BackEndTestSQL.sql
+++ BackEndTestSQL.sql
@@ -1,18 +1,19 @@
 --1 . Create 2 data table in SQLExpress (SQL senteces)
 --A- Countries  (ID int identity,Name varchar,Capital varchar,Population int,LanguageID int)
---B- Languages(ID int identity,Name varchar) foreing key
+--B- Languages(ID int identity,Name varchar) 
 
 CREATE TABLE Languages (
-  LanguageID   integer not null primary key,
+  LanguageID   int primary key,
   LanguageName  varchar(30) not null unique
 );
 
 CREATE TABLE Countries (
-  CountryID    integer not null  primary key,
+  CountryID    integer  primary key,
   CountryName  varchar(30) unique not null,
   CountryCapital  varchar(30) unique not null,
   CountryPopulation numeric(10) not null, 
-  LanguageID integer references Languages(LanguageID)
+  CountryLanguage integer not null,
+  FOREIGN KEY (CountryLanguage) references Languages(LanguageID)
 );
 
 --2. Insert 2 records in each table (SQL senteces)
@@ -21,4 +22,72 @@ insert into Languages values(2, 'English');
 
 insert into Countries values(1, 'Mexico', 'Ciudad de Mexico', 9210000,1);
 insert into Countries values(2, 'United States of America', 'Washington, D.C.', 328240000,2);
+insert into Countries values(3, 'Spain', 'Barcelona', 751000,2);
 
+--3. update the first record the population field by 250000 (SQL senteces) reemplazar el numero
+update Countries
+set CountryPopulation = 250000
+where CountryID = 1;
+
+--4. Delete countries with populations greater than 100,000 (SQL senteces)
+Delete from Countries 
+where CountryPopulation > 100000;
+
+--5. Create SQLSentence  joining the 2 tables.
+Select Countries.CountryName, Languages.LanguageName
+from Countries
+full outer join Languages on Countries.COUNTRYID=Languages.LANGUAGEID;
+
+--6. Create SQL Sentence  grouping  by Languages.
+Select Languages.LanguageName, Count(Countries.CountryName) as NumberOfCountries
+from Countries
+join Languages on Countries.COUNTRYID=Languages.LANGUAGEID
+group by LanguageName;
+
+--7. Create Store Procedures (Insert,Update,Delete,Select all) 
+ALTER PROCEDURE Masterinsertupdatedelete 
+            (CountryID    integer,
+            CountryName  varchar(30),
+            CountryCapital  varchar(30),
+            CountryPopulation numeric(10), 
+            CountryLanguage integer,
+            @StatementType NVARCHAR(20) = '')
+
+AS
+  BEGIN
+      IF @StatementType = 'Insert'
+        BEGIN
+            INSERT INTO Countries
+                        (CountryID,
+                         CountryName,
+                         CountryCapital,
+                         CountryPopulation,
+                         CountryLanguage)
+            VALUES     ( @CountryID,
+                         @CountryName,
+                         @CountryCapital,
+                         @CountryPopulation,
+                         @CountryLanguage)
+        END
+
+      IF @StatementType = 'Select'
+        BEGIN
+            SELECT *
+            FROM   Countries
+        END
+
+      IF @StatementType = 'Update'
+        BEGIN
+            UPDATE Countries
+            SET    CountryName = @CountryName,
+                   CountryCapital = @CountryCapital,
+                   CountryPopulation = @CountryPopulation,
+                   CountryLanguage = @CountryLanguage
+            WHERE  CountryID = @CountryID
+        END
+      ELSE IF @StatementType = 'Delete'
+        BEGIN
+            DELETE FROM Countries
+            WHERE  CountryID = @CountryID
+        END
+  END
